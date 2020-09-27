9a3922f3bc0ddbc7ef8d77c4713aade337858ee1
qiupeng <bmcciscoding@gmail.com>
2020-09-27 11:07:05 +0800

    init project & bubble sort

---
+++ README.md
@@ -0,0 +1,2 @@
+# Go-Algorithm-Practice
+ 学习 Go 语言，用算法练习
---
+++ 排序/bubble.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	arr := []int{10, 33, 45, 88, 76, 23, 32, 55, 66, 92} // 5后面必须有 , 号
+	fmt.Println(bubble(arr))
+}
+
+func bubble(arr []int) []int {
+	count := len(arr)
+	var i, j int
+	for i=0; i<count-1; i++ {
+		for j=0; j<count-1-i; j++ {
+			if (arr[j] > arr[j+1]) {
+				t := arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = t
+			}
+		}
+	}
+	return arr
+}
+
